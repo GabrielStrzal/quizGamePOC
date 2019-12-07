25d3fff562d156e0c706801242295c46b5a5e7c5
Gabriel <gabrielstrzal@gmail.com>
2019-12-07 09:57:50 -0400

    fixing lwjgl problem

--- desktop/src/com/strzal/quiz/desktop/DesktopLauncher.java
+++ desktop/src/com/strzal/quiz/desktop/DesktopLauncher.java
@@ -1,12 +1,14 @@
 package com.strzal.quiz.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.strzal.quiz.QuizGame;
+import com.strzal.quiz.config.GameConfig;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new QuizGame(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setWindowedMode( GameConfig.SCREEN_DESKTOP_DISPLAY_WIDTH, GameConfig.SCREEN_DESKTOP_DISPLAY_HEIGHT);
+		new Lwjgl3Application(new QuizGame(), config);
 	}
 }
