3870e6cccbd6d801b5f35d7ed0f252386ee0b425
Gabriel <gabrielstrzal@gmail.com>
2019-12-07 09:49:25 -0400

    setup basic libgdx

---
+++ desktop/src/com/strzal/quiz/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.strzal.quiz.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.strzal.quiz.QuizGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new QuizGame(), config);
+	}
+}
